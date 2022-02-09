c7a29eee1a3037b09dc0d79a7bc4b5250fa465b5
enkhee-Osiris <enkhee.ag@gmail.com>
2022-02-09 23:03:40 +0800

    init

---
+++ .eslintrc.js
@@ -0,0 +1,64 @@
+module.exports = {
+  root: true,
+  env: { browser: true, es2021: true },
+  extends: [
+    "airbnb",
+    "airbnb-typescript",
+    "airbnb/hooks",
+    "plugin:import/recommended",
+    "plugin:import/typescript",
+    "prettier",
+  ],
+  rules: {
+    "import/extensions": "off",
+    "import/no-unresolved": ["error", { ignore: ["^@/.*$"] }],
+    "import/order": [
+      "error",
+      {
+        alphabetize: { caseInsensitive: true, order: "asc" },
+        groups: ["builtin", "external", "internal", "parent", "sibling", "index"],
+        "newlines-between": "always",
+        pathGroups: [
+          { group: "external", pattern: "react", position: "before" },
+          { group: "parent", pattern: "@/**", position: "before" },
+        ],
+        pathGroupsExcludedImportTypes: ["react"],
+      },
+    ],
+    "import/prefer-default-export": "off",
+    "max-lines": ["error", { max: 100, skipBlankLines: true, skipComments: true }],
+    quotes: "off",
+    "no-console": "off",
+    "react/no-array-index-key": "off",
+    "react/jsx-filename-extension": ["error", { extensions: [".jsx", ".tsx"] }],
+    "react/react-in-jsx-scope": "off",
+    "react/jsx-props-no-spreading": "off",
+    "react/prop-types": "off",
+    "react/require-default-props": "off",
+    "@typescript-eslint/quotes": ["off"],
+  },
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    project: "./tsconfig.json",
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 12,
+    sourceType: "module",
+  },
+  ignorePatterns: ["node_modules", "coverage", ".log", "dist", ".eslintrc.js", "vite.config.ts"],
+  settings: {
+    "import/extensions": [".js", ".jsx", ".ts", ".tsx"],
+    "import/parsers": {
+      "@typescript-eslint/parser": [".ts", ".tsx"],
+    },
+    "import/resolver": {
+      node: {
+        extensions: [".js", ".jsx", ".ts", ".tsx"],
+      },
+    },
+    react: {
+      version: "detect",
+    },
+  },
+};
